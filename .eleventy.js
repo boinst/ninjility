9994d7a0e112bd124005810c4c59289f81a5d151
Ben Ernst <boinst@gmail.com>
2021-07-02 22:07:46 +0930

    Adds "crisp" support to the site.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = {
   dir: {
     input: "src",
-    output: "out"
+    output: "out",
+	includes: "_includes",
+	layouts: "_layouts",
+	data: "_data"
   },
   // pathPrefix: "/ninjility/" // serves with a prefix
 };
---
+++ src/about.md
@@ -0,0 +1,5 @@
+--- 
+layout: base.njk 
+--- 
+# About
+This is my site, I hope you like it.
