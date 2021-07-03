bd2860b5cdaac5787f3dbef787f2cd3a2b3b4eec
Ben Ernst <boinst@gmail.com>
2021-07-03 21:12:46 +0930

    Use the "moment" module to format dates for our blog posts.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,28 @@
-module.exports = {
-  dir: {
-    input: "src",
-    output: "out",
-	includes: "_includes",
-	layouts: "_layouts",
-	data: "_data"
-  },
-  // pathPrefix: "/ninjility/" // serves with a prefix
+// The "moment" library deals with timestamps
+const moment = require('moment');
+
+// We'll use the "english" locale.
+moment.locale('en');
+
+module.exports = function (eleventyConfig) {
+ 
+  // Provides the filter "dateIso"
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+ 
+  // Provides the filter "dateReadable"
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "out",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data"
+    }
+  };
 };
